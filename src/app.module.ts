f8013e0f990f23d6c5a50c97aa4ae9face2cc00f
Salvatore Potrino <s.potrino@sinapsys.it>
2024-07-11 00:27:00 +0200

    Added env variables validation.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { UserManagerModule } from './modules/user-manager/user-manager.module';
+import { AppConstants } from './modules/user-manager/configs/app.constants';
 
 @Module({
   imports: [
@@ -9,16 +10,19 @@ import { UserManagerModule } from './modules/user-manager/user-manager.module';
     ConfigModule.forRoot({
       //load env parameters
       load: [() => ({
-
+        //MS
+        MS_URL: process.env.MS_URL,
+        MS_PROTO_PACKAGE: process.env.MS_PROTO_PACKAGE,
       })],
       validationSchema: Joi.object({
-
+        MS_URL: Joi.string().pattern(AppConstants.IP_PORT_REGEX).required(),
+        MS_PROTO_PACKAGE: Joi.string().required(),
       })
     }),
-    //my modules
-    UserManagerModule
+//my modules
+UserManagerModule
   ],
-  controllers: [],
+controllers: [],
   providers: [],
 })
 export class AppModule { }
